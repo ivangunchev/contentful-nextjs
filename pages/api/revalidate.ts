2ff5a66ebadeba948a969dd71079b97ba7f03b7c
Ivan Gunchev <ivan@stadion.io>
2022-08-16 12:20:57 +0200

    feat: on demand isr homepage and article

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -13,6 +13,7 @@ export default async function handler(
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate("/");
     await res.revalidate(`/news/${path}`);
     return res.json({ revalidated: true });
   } catch (err) {
