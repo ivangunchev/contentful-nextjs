07da44ada4f36555292a5f3c879a48cec70edd23
Ivan Gunchev <ivan@stadion.io>
2022-08-23 21:11:09 +0200

    fix: next cors

--- pages/api/proxy/folders.ts
+++ pages/api/proxy/folders.ts
@@ -1,11 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 import getAccessToken from "../../../utils/brightcove/getAccessToken";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
   try {
     const { access_token: accessToken, token_type: tokenType } =
       await getAccessToken();
