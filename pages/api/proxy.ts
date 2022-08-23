98dc8abe5ebb9b91d81b620d7d69e60951649d79
Ivan Gunchev <ivan@stadion.io>
2022-08-23 17:27:40 +0200

    fix: proxy

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -1,51 +1,27 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-function extractApiRoute(req) {
-  return `${req.params["0"]}?${new URLSearchParams(req.query).toString()}`;
-}
-
-function getAccessToken(clientId, clientSecret) {
-  const encodedData = Buffer.from(`${clientId}:${clientSecret}`).toString(
-    "base64"
-  );
-
-  return fetch("https://oauth.brightcove.com/v4/access_token", {
-    method: "POST",
-    body: "grant_type=client_credentials",
-    headers: {
-      Authorization: `Basic ${encodedData}`,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  }).then((response) => response.json());
-}
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import getAccessToken from "../../utils/brightcove/getAccessToken";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    // eslint-disable-next-line camelcase
-    const { access_token, token_type } = await getAccessToken(
-      process.env.BRIGHTCOVE_CLIENT_ID,
-      process.env.BRIGHTCOVE_CLIENT_SECRET
-    );
+    const { access_token: accessToken, token_type: tokenType } =
+      await getAccessToken();
+    res.json({ accessToken, tokenType });
 
     const response = await fetch(
-      `https://cms.api.brightcove.com/v1/accounts/${
-        process.env.BRIGHTCOVE_ACCOUNT_ID
-      }/${extractApiRoute(req)}`,
+      `https://cms.api.brightcove.com/v1/accounts/${process.env.BRIGHTCOVE_ACCOUNT_ID}/`,
       {
         headers: {
-          // eslint-disable-next-line camelcase
-          Authorization: `${token_type} ${access_token}`,
+          Authorization: `${tokenType} ${accessToken}`,
         },
       }
     );
     res.json({ response: "ok" });
     res.json(await response.json());
   } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return res.status(500).send("Error with the proxy");
+    console.error(err);
   }
 }
